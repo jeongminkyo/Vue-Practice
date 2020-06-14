cc202ae1043a77e7a2ec80361c2751059b174c9f
Jeong Minkyo <redwonder@naver.com>
2020-06-14 18:58:58 +0900

    delete Method 수정

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -21,6 +21,7 @@ import cryptoRandomString from 'crypto-random-string'
 import _cloneDeep from 'lodash/cloneDeep'
 import _find from 'lodash/find'
 import _assign from 'lodash/assign'
+import _findIndex from 'lodash/findIndex'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -88,8 +89,14 @@ export default {
             _assign(foundTodo, value)
             
         },
-        deleteTodo () {
-            console.log('delete todo')
+        deleteTodo (todo) {
+            this.db
+                .get('todos')
+                .remove({ id: todo.id })
+                .write()
+
+            const foundIndex = _findIndex(this.todos, { id: todo.id })
+            this.$delete(this.todos, foundIndex)
         }
     }
 }
