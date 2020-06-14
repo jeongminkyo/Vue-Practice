730924a9537090af03cb98b5e20df14cab4870e6
Jeong Minkyo <redwonder@naver.com>
2020-06-14 16:11:56 +0900

    Todo Component 연결

---
+++ components/TodoCreator.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Todo Creator</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ components/TodoItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Todo Item</h2>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
