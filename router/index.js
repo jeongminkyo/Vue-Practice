e17b587cbb0a85523559471c1a9ce1b0218c678b
Jeong Minkyo <redwonder@naver.com>
2020-06-16 00:03:11 +0900

    router param으로 항목제어 추가

--- router/index.js
+++ router/index.js
@@ -10,16 +10,26 @@ Vue.use(VueRouter)
 const routes = [
     //config
     {
+        name: 'index',
         path: '/',
         component: Home
     },
     {
+        name: 'about',
         path: '/about',
         component: About
     },
     {
+        name: 'todos',
         path: '/todos',
-        component: TodoApp
+        redirect: 'todos/all',
+        component: TodoApp,
+        children: [
+            {
+                name: 'todos-filter',
+                path: ':id'
+            }
+        ]
     }
 ]
 
