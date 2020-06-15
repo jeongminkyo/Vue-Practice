422b661f8756ea942975869e0a32a3f2e59376a9
Jeong Minkyo <redwonder@naver.com>
2020-06-15 23:04:37 +0900

    홈으로 버튼 추가

---
+++ views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="about">
+        <div>
+            <h2>Jeong Min-Kyo</h2>
+            <h2>jeong.minkyo13@gmail.com</h2>
+            <h2>010-9429-2658</h2>
+        </div>
+    </section>
+</template>
